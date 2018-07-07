21c7a880506cf5e0beac4f0bcca0e5065a92d995
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2018-07-07 19:55:07 +0200

    Remove required from settings properties

--- server/src/models/settings.js
+++ server/src/models/settings.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const settingsSchema = mongoose.Schema({
-  hourlyWage: { type: Number, required: true, default: 0 },
-  person: { type: String, required: true, default: '' },
+  hourlyWage: { type: Number, default: 0 },
+  person: { type: String, default: '' },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
