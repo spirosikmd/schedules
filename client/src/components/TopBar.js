9ae58a516f0d8f55bb6b446bdafbbfb64348ea64
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2018-09-29 16:28:11 +0200

    Style top bar

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -5,36 +5,62 @@ import Typography from '@material-ui/core/Typography';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 import { GoogleLogout } from 'react-google-login';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 class TopBar extends PureComponent {
   render() {
-    const { user, onMenuIconClick, onGoogleLogoutSuccess } = this.props;
+    const {
+      user,
+      onMenuIconClick,
+      onGoogleLogoutSuccess,
+      classes,
+    } = this.props;
 
     return (
-      <AppBar position="static" style={{ flexGrow: 1 }}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="Menu"
-            onClick={onMenuIconClick}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="title" color="inherit" style={{ flexGrow: 1 }}>
-            Welcome to {user.firstName} schedule
-          </Typography>
-          <Button
-            color="inherit"
-            component={GoogleLogout}
-            onLogoutSuccess={onGoogleLogoutSuccess}
-          >
-            Logout
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+              onClick={onMenuIconClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.grow}
+            >
+              Welcome to {user.firstName} schedule
+            </Typography>
+            <Button
+              color="inherit"
+              component={GoogleLogout}
+              onLogoutSuccess={onGoogleLogoutSuccess}
+            >
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
   }
 }
 
-export default TopBar;
+export default withStyles(styles)(TopBar);
