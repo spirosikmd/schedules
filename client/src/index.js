bf6c269ea156bd959b3652b8e86e787f49c61244
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2018-09-29 14:48:00 +0200

    Remove global index css file

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
