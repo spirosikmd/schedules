86fe17f7a8cca7d56b613dbb0d43b4b2add6b40c
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2018-11-04 09:49:49 +0100

    Use a dialog for uploading a new schedule

--- client/src/components/ScheduleFileUploadForm.js
+++ client/src/components/ScheduleFileUploadForm.js
@@ -1,8 +1,10 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class ScheduleFileUploadForm extends PureComponent {
   constructor(props) {
@@ -11,7 +13,19 @@ class ScheduleFileUploadForm extends PureComponent {
     this.handleSubmit = this.handleSubmit.bind(this);
 
     this.fileInputRef = React.createRef();
+
+    this.state = {
+      open: false,
+    };
   }
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
   handleSubmit(event) {
     event.preventDefault();
@@ -25,38 +39,42 @@ class ScheduleFileUploadForm extends PureComponent {
     }
 
     this.props.onSubmit(file);
+    this.handleClose();
 
     fileInput.value = '';
   }
 
   render() {
+    const { open } = this.state;
+
     return (
-      <Grid container alignItems="center" justify="flex-start" spacing={8}>
-        <Grid item>
-          <Typography>Upload a schedule:</Typography>
-        </Grid>
-        <Grid item>
-          <form onSubmit={this.handleSubmit}>
-            <Grid
-              container
-              alignItems="center"
-              justify="flex-start"
-              spacing={8}
-            >
-              <Grid item>
-                <TextField
-                  type="file"
-                  id="scheduleFile"
-                  inputRef={this.fileInputRef}
-                />
-              </Grid>
-              <Grid item>
-                <Button type="submit">Upload</Button>
-              </Grid>
-            </Grid>
-          </form>
-        </Grid>
-      </Grid>
+      <Fragment>
+        <Button onClick={this.handleClickOpen}>Upload</Button>
+        <Dialog
+          open={open}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">
+            Upload a new schedule
+          </DialogTitle>
+          <DialogContent>
+            <TextField
+              type="file"
+              id="scheduleFile"
+              inputRef={this.fileInputRef}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleSubmit} color="primary">
+              Upload
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Fragment>
     );
   }
 }
