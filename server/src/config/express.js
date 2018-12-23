1e6959e0265f716dd382b1faec762042067397aa
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2018-12-23 20:17:56 +0100

    Use gzip compression on the server

--- server/src/config/express.js
+++ server/src/config/express.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 module.exports = function(app) {
   app.use(passport.initialize());
@@ -10,6 +11,8 @@ module.exports = function(app) {
   app.use(bodyParser.json());
 
   app.use(cookieParser());
+
+  app.use(compression());
 
   app.use(express.static(path.join(__dirname, '../../../client/build')));
 };
