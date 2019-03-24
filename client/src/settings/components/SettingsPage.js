67ebd17982da80ea4a414c5e1d550bcb775a74af
Spyros Ioakeimidis <spyrosikmd@gmail.com>
2019-03-24 12:45:18 +0100

    Add paper styling to settings

--- client/src/settings/components/SettingsPage.js
+++ client/src/settings/components/SettingsPage.js
@@ -2,28 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { navigate } from '@reach/router';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 import DeleteUserDialog from './DeleteUserDialog';
 import { deleteUserAndLogout } from '../actions';
 import withAuth from '../../shared/components/withAuth';
 
-const SettingsPage = React.memo(({ deleteUser: deleteUserAndLogout }) => {
-  const handleDeleteClick = async () => {
-    await deleteUserAndLogout();
-    navigate('/');
-  };
-
-  return <DeleteUserDialog onDeleteClick={handleDeleteClick} />;
+const styles = theme => ({
+  dangerZone: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
 });
+
+const SettingsPage = React.memo(
+  ({ deleteUser: deleteUserAndLogout, classes }) => {
+    const handleDeleteClick = async () => {
+      await deleteUserAndLogout();
+      navigate('/');
+    };
+
+    return (
+      <Paper className={classes.dangerZone}>
+        <Typography variant="h5" gutterBottom>
+          Danger zone
+        </Typography>
+        <DeleteUserDialog onDeleteClick={handleDeleteClick} />
+      </Paper>
+    );
+  }
+);
 
 SettingsPage.propTypes = {
   deleteUserAndLogout: PropTypes.func.isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
 const mapDispatchToProps = {
   deleteUserAndLogout,
 };
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(withAuth(SettingsPage));
+export default withStyles(styles)(
+  connect(
+    null,
+    mapDispatchToProps
+  )(withAuth(SettingsPage))
+);
